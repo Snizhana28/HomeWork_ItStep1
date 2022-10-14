56b5bcd2f729cfc4c41065cc19b5d8d1104c6d24
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 18:09:39 +0300

    Done Task 2_1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
+/*Task 1. The user enters the time from the keyboard in seconds that have passed since the beginning of the day. Display the current one
+time in hours, minutes and seconds. Count how much
+hours, minutes and seconds left until midnight.*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
-
+	int second;
+	cout << "Enter the time in seconds since the start of the day : ";
+	cin >> second;
+	int hours = second / 3600;
+	int min = second / 60 - hours * 60;
+	int sec = second - min * 60 - hours * 3600;
+	cout << hours << " hours " << min << " min " << sec << " sec ";
 	return 0;
 }
